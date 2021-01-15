64de62795a83bfd396d8e99bc630d69f9a9e1bc3
mikolaj.mak00 <mikolaj.mak00@gmail.com>
2021-01-15 20:43:34 +0100

    Dodano Unity i kontroler dla prowadzących

---
+++ ProjektTNAI/Controllers/ProwadzacyController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetTNAI.Entities;
+using ProjetTNAI.Entities.Models;
+using ProjetTNAI.DataAccessLayer.Repositories.Abstract;
+
+namespace ProjektTNAI.Controllers
+{
+    public class ProwadzacyController : Controller
+    {
+        private readonly IProwadzacyRepository _prowadzacyRepository;
+
+        public ProwadzacyController(IProwadzacyRepository prowadzacyRepository)
+        {
+            _prowadzacyRepository = prowadzacyRepository;
+        }
+
+        // GET: Prowadzacy
+        public async Task<ActionResult> Index()
+        {
+            var prowadzacy = await _prowadzacyRepository.GetWieluProwadzacychAsync();
+            return View(prowadzacy);
+        }
+
+        // GET: Prowadzacy/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prowadzacy prowadzacy = await _prowadzacyRepository.GetProwadzacyAsync(id.Value);
+            if (prowadzacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prowadzacy);
+        }
+
+        // GET: Prowadzacy/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Prowadzacy/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Imie,Nazwisko,Email")] Prowadzacy prowadzacy)
+        {
+            if (ModelState.IsValid)
+            {
+                await _prowadzacyRepository.ZapiszProwadzacyAsync(prowadzacy);
+                return RedirectToAction("Index");
+            }
+
+            return View(prowadzacy);
+        }
+
+        // GET: Prowadzacy/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prowadzacy prowadzacy = await _prowadzacyRepository.GetProwadzacyAsync(id.Value);
+            if (prowadzacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prowadzacy);
+        }
+
+        // POST: Prowadzacy/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Imie,Nazwisko,Email")] Prowadzacy prowadzacy)
+        {
+            if (ModelState.IsValid)
+            {
+                await _prowadzacyRepository.ZapiszProwadzacyAsync(prowadzacy);
+                return RedirectToAction("Index");
+            }
+            return View(prowadzacy);
+        }
+
+        // GET: Prowadzacy/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prowadzacy prowadzacy = await _prowadzacyRepository.GetProwadzacyAsync(id.Value);
+            if (prowadzacy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prowadzacy);
+        }
+
+        // POST: Prowadzacy/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Prowadzacy prowadzacy = await _prowadzacyRepository.GetProwadzacyAsync(id);
+            await _prowadzacyRepository.UsunProwadzacyAsync(prowadzacy);
+            return RedirectToAction("Index");
+        }
+    }
+}
